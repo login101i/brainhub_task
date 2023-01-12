6ce9e4e830247f123e08f4fb8df991e9ed47a751
mkruszyniak <maciejkruszyniak@gmail.com>
2023-01-12 09:28:48 +0100

    polishing

--- frontend/src/components/EventsList/EventsList.jsx
+++ frontend/src/components/EventsList/EventsList.jsx
@@ -1,24 +1,9 @@
-import React, { useState, useEffect } from 'react';
 import { SingleEvent } from '..';
 import { EventsContainer, EventsTitle } from './EventsList.styles';
-import PropTypes from 'prop-types';
 import { useEventContext } from '../../appState/event.context';
-import axios from 'axios';
 
 export const EventsList = () => {
-  const [events, setEvents] = useState([]);
-
-  const getEvents = async () => {
-    try {
-      const { data } = await axios.get('http://127.0.0.1:4000/api/v1/events');
-      setEvents(data.eventsList);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  useEffect(() => {
-    getEvents();
-  }, []);
+  const { events } = useEventContext();
 
   return (
     <>
